e2a2a3673cc71d19ba1347087ff2f7ed32aea27b
Asiri Dissanayaka <asiridissa@live.com>
2023-10-26 16:13:35 +0530

    Schema file added back. on debug it allws to use local file

--- RideScheduler.TDS.Validator/JSON.cs
+++ RideScheduler.TDS.Validator/JSON.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,9 +15,24 @@ namespace RideScheduler.TDS.Validator
         public static string GetSchema()
         {
             var configuration = new ConfigurationBuilder().AddJsonFile($"appsettings.json").Build();
+#if DEBUG
+            var useLocal = Convert.ToBoolean(configuration["UseLocalSchema"]);
+            if (useLocal)
+            {
+                return File.ReadAllText(Directory.GetParent(Assembly.GetCallingAssembly().Location) + "/Schema/schema.json");
+            }
+            else
+            {
+                string url = configuration["JSONSchemaURL"];
+                var schemaJson = new WebClient().DownloadString(url);
+                return schemaJson;
+            }
+#else            
+
             string url = configuration["JSONSchemaURL"];
             var schemaJson = new WebClient().DownloadString(url);
             return schemaJson;
+#endif
         }
     }
 }
